872702f4b09581f74122f123f2aad0ca8c0b3a9c
oiuy-888 <oiuy-888@163.comm>
2020-09-26 23:48:40 +0800

    增加markdown预览功能

--- src/views/markdown/index.vue
+++ src/views/markdown/index.vue
@@ -1,65 +1,29 @@
 <template>
-  <div class="components-container">
-    <div class="editor-container">
-      <el-tag class="tag-title">
-        Markdown Mode:
-      </el-tag>
-      <markdown-editor ref="markdownEditor" v-model="content2" :options="{hideModeSwitch:true,previewStyle:'tab'}" height="200px" />
-    </div>
-    <div class="editor-container">
-      <el-tag class="tag-title">
-        I18n:
-      </el-tag>
-      <markdown-editor ref="markdownEditor" v-model="content4" :language="language" height="300px" />
+  <div class="mavonEditor">
+    <div>
+      <mavon-editor style="height:600px" v-model="doc"> </mavon-editor>
     </div>
   </div>
 </template>
 
 <script>
-import MarkdownEditor from '@/components/MarkdownEditor'
+  import {mavonEditor} from "mavon-editor"
+  import "mavon-editor/dist/css/index.css"
 
-const content = `
-**This is test**
-
-在线编辑
-
-`
-export default {
-  name: 'MarkdownDemo',
-  components: { MarkdownEditor },
-  data() {
-    return {
-      content1: content,
-      content2: content,
-      content3: content,
-      content4: content,
-      html: '',
-      languageTypeList: {
-        'en': 'en_US',
-        'zh': 'zh_CN',
-        'es': 'es_ES'
+  export default {
+    name: "Create",
+    components: {mavonEditor},
+    data(){
+      return {
+        doc: '暂无法编辑图片预览'
       }
     }
-  },
-  computed: {
-    language() {
-      return this.languageTypeList['en']
-    }
-  },
-  methods: {
-    getHtml() {
-      this.html = this.$refs.markdownEditor.getHtml()
-      console.log(this.html)
-    }
   }
-}
 </script>
 
 <style scoped>
-  .editor-container{
-    margin-bottom: 30px;
-  }
-  .tag-title{
-    margin-bottom: 5px;
+  .mavonEditor {
+    width: 100%;
+    height: 100%;
   }
 </style>
