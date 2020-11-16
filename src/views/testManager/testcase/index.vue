173d6193c4ed35fadf776532b19d3fb99963d39f
lishuai228 <lishuai228@jd.com>
2020-11-16 20:40:11 +0800

    添加背景图片示例

--- src/views/testManager/testcase/index.vue
+++ src/views/testManager/testcase/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="tableDate">
-    <steps v-bind="stepsObj"/>
-    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+    <div class="pull-height animated" :style="{backgroundImage:'url('+url+')'}" style="background-size: 100% 100%">
+      <div class="index">
+        <steps v-bind="stepsObj" />
+        <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import steps from "@/components/steps";
+import steps from '@/components/steps'
+import bg from '@/icons/images/bg.jpeg'
 
 export default {
   components: {
@@ -15,18 +20,27 @@ export default {
   data() {
     return {
       tableData: [],
-      stepsObj:{
+      url: bg,
+      stepsObj: {
         active: 0,
-        title:'创建插件',
-        tiptitle:['选择插件类型','填写信息','完成']
-      },
+        title: '创建插件',
+        tiptitle: ['选择插件类型', '填写信息', '完成']
+      }
     }
   },
   methods: {
     next() {
-      this.$router.push({path: '/testManager/addplug'})
-      if (this.active++ > 2) this.active = 0;
+      this.$router.push({ path: '/testManager/addplug' })
+      if (this.active++ > 2) this.active = 0
     }
   }
 }
 </script>
+
+<style>
+.index {
+    filter:alpha(Opacity=95);
+    -moz-opacity:0.95;
+    opacity: 0.95;
+    }
+</style>
--- src/views/testManager/testplug/addplugthree.vue
+++ src/views/testManager/testplug/addplugthree.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="tableDate">
-    <steps v-bind="stepsObj"/>
+    <steps v-bind="stepsObj" />
     <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
   </div>
 </template>
 
 <script>
-import steps from "@/components/steps";
+import steps from '@/components/steps'
 
 export default {
   components: {
@@ -15,16 +15,16 @@ export default {
   data() {
     return {
       tableData: [],
-      stepsObj:{
+      stepsObj: {
         active: 2,
-        title:'创建插件',
-        tiptitle:['选择插件类型','填写信息','完成']
-      },
+        title: '创建插件',
+        tiptitle: ['选择插件类型', '填写信息', '完成']
+      }
     }
   },
   methods: {
     next() {
-      this.$router.push({path: '/testManager/testsuit/index'})
+      this.$router.push({ path: '/testManager/testplug/index' })
     }
   }
 }
