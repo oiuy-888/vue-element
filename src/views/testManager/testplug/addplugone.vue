6d913ae301f7c40da55cbe99653be476905dc95c
lishuai228 <lishuai228@jd.com>
2020-11-25 19:05:42 +0800

    改造用例集菜单

--- src/views/testManager/testplug/addplugone.vue
+++ src/views/testManager/testplug/addplugone.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="tableDate">
-    <steps v-bind="stepsObj"/>
+    <steps v-bind="stepsObj" />
+    <div class="text-desc">
+      选择插件类型并自定义名称
+    </div>
+    <el-form :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" style="width:30%;" placeholder="审批人" />
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai" />
+          <el-option label="区域二" value="beijing" />
+        </el-select>
+      </el-form-item>
+    </el-form>
     <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
   </div>
 </template>
 
 <script>
-import steps from "@/components/steps";
+import steps from '@/components/steps'
 
 export default {
   components: {
@@ -15,17 +29,31 @@ export default {
   data() {
     return {
       tableData: [],
-      stepsObj:{
+      stepsObj: {
         active: 0,
-        title:'创建插件',
-        tiptitle:['选择插件类型','填写信息','完成']
+        title: '创建插件',
+        tiptitle: ['选择插件类型', '填写信息', '完成']
       },
+      formInline: {
+        user: '',
+        region: ''
+      }
     }
   },
   methods: {
     next() {
-      this.$router.push({path: '/testManager/addplugtwo'})
+      this.$router.push({ path: '/testManager/addplugtwo' })
     }
   }
 }
 </script>
+
+<style>
+.text-desc {
+  text-align: center;
+  font-size: 20px;
+}
+.plug-form {
+
+}
+</style>
--- src/views/testManager/testplug/index.vue
+++ src/views/testManager/testplug/index.vue
@@ -1,36 +1,35 @@
 <template>
   <div class="tableDate">
     <el-container style="height: 100%; border: 5px solid #eee;">
-      <el-header align="center">插件选型</el-header>
+      <el-header align="center">插件选型列表</el-header>
       <el-main>
-        <el-button style="margin-top: 40px;" @click="next">下一步</el-button>
+        <el-button style="margin-top: 40px;" @click="next">创建插件</el-button>
       </el-main>
-      <el-footer align="center">插件类型问题，可查阅cf文档</el-footer>
+      <el-footer class="footer-font-size">插件类型问题，可查阅cf文档</el-footer>
     </el-container>
   </div>
 </template>
 
 <script>
-import steps from "@/components/steps";
 
 export default {
-  components: {
-    steps
-  },
   data() {
     return {
-      tableData: [],
-      stepsObj:{
-        active: 0,
-        title:'创建插件',
-        tiptitle:['选择插件类型','填写信息','完成']
-      },
+      tableData: []
     }
   },
   methods: {
     next() {
-      this.$router.push({path: '/testManager/addplugone'})
+      this.$router.push({ path: '/testManager/addplugone' })
     }
   }
 }
 </script>
+
+<style>
+.footer-font-size {
+  font-size: 10px;
+  line-height:100px;
+  text-align: center;
+}
+</style>
